14974c736d5e0f5720ac27443e61e58d930c5477
trongtuyen <trongtuyen451@gmail.com>
2024-11-15 10:14:56 +0700

    Bai17Update1

--- src/test/java/com/BookingTest.java
+++ src/test/java/com/BookingTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.utils.Utils;
 
 import com.utils.BasicTest;
 
@@ -15,44 +16,29 @@ public class BookingTest extends BasicTest {
 
     @Test(priority = 1)
     public void registerTestFailure() throws Exception {
-        // Launch website
         String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-
         WebElement emailInput = driver.findElement(By.xpath("//input[@id='username']"));
         emailInput.sendKeys("trongtuyen451@gmail.com");
         WebElement passInput = driver.findElement(By.xpath("//input[@id='password']"));
         passInput.sendKeys("Admin@123456");
-
         WebElement loginBtn = driver.findElement(By.xpath("//button[@name='login']"));
         loginBtn.click();
-        //WebElement errorrMessageText = driver.findElement(By.xpath("//li[contains(@class,'logout')]"));
-       // Assert.assertTrue(errorrMessageText.isDisplayed());
-
         WebElement seachBox = driver.findElement(By.xpath("//input[@placeholder='Tìm kiếm...'][1]"));
         seachBox.sendKeys("Mercedes");
-        WebElement optionFirts = driver.findElement(By.xpath("(//div[@class='fs-sremain fs-sremain-products']/a)[1]"));
-        // Thread.sleep(5000);
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        // Chờ cho đến khi phần tử có ID 'elementId' hiển thị
-        // WebElement element =
-        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='fs-sremain
-        // fs-sremain-products']/a)[1]")));
+        Utils.hardWait();
+        WebElement optionFirts = driver.findElement(By.xpath("//a[contains(text(),'Bơm nước xe Mercedes SLK200, SLK300, GLK200, E260, C350-2742000207')]"));
         optionFirts.click();
         WebElement selectOption = driver.findElement(By.xpath("//select[@id='pa_xuat-xu']/option[@value='germany']"));
         selectOption.click();
         WebElement btnPlus = driver.findElement(By.xpath("//button[@class='plus']"));
         btnPlus.click();
-        WebElement btnAdd = driver.findElement(By.xpath("//button[contains(@class,'single_add_to_cart_button')"));
+        WebElement btnAdd = driver.findElement(By.xpath("//button[contains(@class,'single_add_to_cart_button')]"));
         btnAdd.click();
-
         String urlCard = "https://bantheme.xyz/hathanhauto/gio-hang/";
         Assert.assertEquals(driver.getCurrentUrl(), urlCard);
-
         WebElement iconCard = driver.findElement(By.xpath("//div[@class='d-table-cell link-cart']//a/b[text()='2']"));
         Assert.assertTrue(iconCard.isDisplayed());
     }
-
 }
